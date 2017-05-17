d743208539f83cab643ab1788cc802b74a265616
Samuel Pattuzzi <sam@pattuzzi.it>
2017-05-17 11:16:45 +0100

    Extracted Navigation pack.

---
+++ Navigation Pack/Assets/Character/Character.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+public class Character : MonoBehaviour {
+
+    Animator playerAnimator;
+    Rigidbody rb;
+    public float upJumpSpeed = 5;
+    public float forwardJumpSpeed = 1;
+
+	// Use this for initialization
+	void Awake () {
+        playerAnimator = GetComponent<Animator>();
+        playerAnimator.applyRootMotion = true;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        playerAnimator.applyRootMotion = Grounded();
+        playerAnimator.SetBool("grounded", Grounded());
+    }
+
+    public bool Grounded()
+    {
+        return Mathf.Abs(Vector3.Dot(rb.velocity, Vector3.up)) < 0.01;
+    }
+
+    public void Jump()
+    {
+        if (Grounded())
+        {
+            playerAnimator.SetTrigger("jump");
+            rb.velocity += Vector3.up * upJumpSpeed + transform.forward * forwardJumpSpeed;
+        }
+    }
+
+    public void Duck()
+    {
+        playerAnimator.SetTrigger("duck");
+    }
+
+    public void SetTurnSpeed(float speed)
+    {
+        playerAnimator.SetFloat("turnSpeed", speed);
+    }
+
+    public void SetForwardSpeed(float speed)
+    {
+        playerAnimator.SetFloat("forwardSpeed", speed);
+    }
+}
---
+++ Navigation Pack/Assets/Character/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Character))]
+public class PlayerController : MonoBehaviour {
+
+    Character character;
+
+	// Use this for initialization
+	void Start () {
+        character = GetComponent<Character>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var v = Input.GetAxis("Vertical");
+        var h = Input.GetAxis("Horizontal");
+
+        character.SetForwardSpeed(v);
+        character.SetTurnSpeed(h);
+
+        if (Input.GetButtonDown("Duck"))
+        {
+            character.Duck();
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            character.Jump();
+        }
+    }
+}
