319cbd52c3a4c58a517bbbb758aae06683adf3fa
Samuel Pattuzzi <sam@pattuzzi.it>
2017-05-17 11:10:31 +0100

    Put the asset from Cert into version control.

---
+++ Player/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour {
+
+    Animator playerAnimator;
+    Rigidbody rb;
+    public float upJumpSpeed = 5;
+    public float forwardJumpSpeed = 1;
+
+	// Use this for initialization
+	void Awake () {
+        playerAnimator = GetComponent<Animator>();
+        playerAnimator.applyRootMotion = true;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        var v = Input.GetAxis("Vertical");
+        var h = Input.GetAxis("Horizontal");
+
+
+        playerAnimator.SetFloat("forwardSpeed", v);
+        playerAnimator.SetFloat("turnSpeed", h);
+
+        if (Input.GetButtonDown("Duck"))
+        {
+            playerAnimator.SetTrigger("duck");
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerAnimator.SetTrigger("jump");
+            rb.velocity += Vector3.up * upJumpSpeed + transform.forward * forwardJumpSpeed;
+        }
+
+        bool grounded = Mathf.Abs(Vector3.Dot(rb.velocity, Vector3.up)) < 0.01;
+        playerAnimator.applyRootMotion = grounded;
+        playerAnimator.SetBool("grounded", grounded);
+    }
+}
---
+++ Spinner-Script/Spinner Script/Spinner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+
+	public float degreesPerSecond = 10;
+	public float rotationTime = 2;
+
+	float rotationStopTime = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rotationStopTime > Time.time) {
+			transform.Rotate (new Vector3 (0, 0, degreesPerSecond) * Time.deltaTime);
+		}
+	}
+
+	public void StartSpinning () {
+		rotationStopTime = Time.time + rotationTime;
+	}
+
+	public void StopSpinning () {
+		rotationStopTime = 0;
+	}
+
+	public void SetRotationSpeed(float dps) {
+		degreesPerSecond = dps;
+	}
+
+	public void SetRotationTime(float time) {
+		rotationTime = time;
+	}
+}
