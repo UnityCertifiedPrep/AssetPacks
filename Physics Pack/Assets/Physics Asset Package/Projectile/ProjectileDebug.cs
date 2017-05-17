67e745c182bb5dac8f55569ae601b566b0765173
Samuel Pattuzzi <sam@pattuzzi.it>
2017-05-17 11:52:11 +0100

    One last removal of dead code.

--- Physics Pack/Assets/Physics Asset Package/Projectile/ProjectileDebug.cs
+++ Physics Pack/Assets/Physics Asset Package/Projectile/ProjectileDebug.cs
@@ -4,13 +4,6 @@ using System.Collections;
 public class ProjectileDebug : MonoBehaviour
 {
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         print(GetComponent<Rigidbody>().velocity.magnitude);
